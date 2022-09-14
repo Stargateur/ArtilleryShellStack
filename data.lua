58f5078d9e78b00713539ff097869b5a3ceba578
Antoine <plaskowski.stanislas@gmail.com>
2022-09-14 04:05:08 +0200

    0.0.0

---
+++ data.lua
@@ -0,0 +1,4 @@
+data.raw.ammo["artillery-shell"].stack_size =
+  settings.startup["artillery-shell-stack-size"].value
+data.raw["artillery-turret"]["artillery-turret"].ammo_stack_limit =
+  settings.startup["artillery-turret-ammo-stack-limit"].value
